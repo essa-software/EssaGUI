12503827c8948b8ff13066e494ef89f6ba8b707e
Maciej <sppmacd@pm.me>
2022-07-06 14:25:07 +0200

    Build: Download EssaUtil automatically

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ if [ ! -e build ]; then
     mkdir build
 fi
 cd build
+
+rm -rf util
+git clone https://github.com/essa-software/util
+cd util
+CMAKE_OPTIONS=-DESSAUTIL_ENABLE_SFML=1 ./install.sh
+cd ..
+
 cmake .. -GNinja
 ninja
 sudo ninja install
