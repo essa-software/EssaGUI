b3d124e9b010624f78a72e670dd800d686082170
Maciej <sppmacd@pm.me>
2022-07-18 16:23:06 +0200

    GUI: Improve appearance of tooltips

--- EssaGUI/gui/Tooltip.cpp
+++ EssaGUI/gui/Tooltip.cpp
@@ -9,16 +9,22 @@ namespace GUI {
 void TooltipOverlay::draw() {
     TextDrawOptions text;
     text.font_size = 15;
-    text.fill_color = Util::Colors::black;
+    text.fill_color = theme().tooltip.text;
     text.text_align = Align::CenterLeft;
 
     auto bounds = window().calculate_text_size(m_tooltip.text, Application::the().font, text);
 
-    auto x_pos = std::min<float>(window().size().x() - (bounds.x() + 10), position().x() - 5);
+    RectangleDrawOptions background;
+    background.fill_color = theme().tooltip.background;
+    background.outline_color = theme().tooltip.foreground;
+    background.outline_thickness = -1;
+
+    auto x_pos = std::min<float>(window().size().x() - (bounds.x() + 10), position().x() - 10);
     window().draw_rectangle({
-        { x_pos, position().y() - 10 },
-        { bounds.x() + 10, bounds.y() + 10 },
-    });
+                                { x_pos, position().y() - 15 },
+                                { bounds.x() + 20, bounds.y() + 12 },
+                            },
+        background);
 
     // FIXME: Text size is calculated 2x
     window().draw_text_aligned_in_rect(m_tooltip.text, rect(), Application::the().font, text);
