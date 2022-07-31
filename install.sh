73f4eb8bed1741a5c5833a18dcaa9f554aece8d4
Maciej <sppmacd@pm.me>
2022-07-31 17:39:19 +0200

    Build: Don't install llgl anymore

--- install.sh
+++ install.sh
@@ -10,12 +10,6 @@ cd util
 ./install.sh || exit 1
 cd ..
 
-rm -rf llgl
-git clone https://github.com/sppmacd/llgl
-cd llgl
-./install.sh || exit 1
-cd ..
-
 cmake .. -GNinja
 ninja
 sudo ninja install
