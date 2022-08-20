da89b6da0f557b60c2c47f3fe378489b61b1ce12
Maciej <sppmacd@pm.me>
2022-08-20 16:13:55 +0200

    LLGL/OpenGL: Add Projection::set_viewport

--- LLGL/OpenGL/Projection.hpp
+++ LLGL/OpenGL/Projection.hpp
@@ -20,6 +20,8 @@ public:
     Util::Recti viewport() const { return m_viewport; }
     Util::Matrix4x4f matrix() const { return m_matrix; }
 
+    void set_viewport(Util::Recti v) { m_viewport = v; }
+
 private:
     Util::Matrix4x4f m_matrix;
     Util::Recti m_viewport;
