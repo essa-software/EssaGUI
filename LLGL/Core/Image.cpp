8877d558a330e10d6f109350c320b151975c924a
Maciej <sppmacd@pm.me>
2022-09-01 20:14:14 +0200

    LLGL/Core: Use std::fill in Image::create_filled_with_color
    This makes creating much faster since it can be better optimized
    by compiler/CPU.

--- LLGL/Core/Image.cpp
+++ LLGL/Core/Image.cpp
@@ -5,14 +5,12 @@
 
 namespace llgl {
 
-Image::Image(Image&& other)
-{
+Image::Image(Image&& other) {
     m_pixels = std::exchange(other.m_pixels, nullptr);
     m_size = std::exchange(other.m_size, {});
 }
 
-Image& Image::operator=(Image&& other)
-{
+Image& Image::operator=(Image&& other) {
     if (this == &other)
         return *this;
 
@@ -23,25 +21,18 @@ Image& Image::operator=(Image&& other)
     return *this;
 }
 
-Image Image::create_uninitialized(Util::Vector2u size)
-{
+Image Image::create_uninitialized(Util::Vector2u size) {
     Image image { size };
     return image;
 }
 
-Image Image::create_filled_with_color(Util::Vector2u size, Util::Color color)
-{
+Image Image::create_filled_with_color(Util::Vector2u size, Util::Color color) {
     Image image { size };
-    for (unsigned x = 0; x < image.size().x(); x++) {
-        for (unsigned y = 0; y < image.size().y(); y++) {
-            image.set_pixel({ x, y }, color);
-        }
-    }
+    std::fill(image.m_pixels, image.m_pixels + image.pixel_count(), color);
     return image;
 }
 
-void Image::insert_image_in_bounds(Image const& other, Util::Vector2u where)
-{
+void Image::insert_image_in_bounds(Image const& other, Util::Vector2u where) {
     assert(where.x() + other.size().x() <= m_size.x());
     assert(where.y() + other.size().y() <= m_size.y());
     for (unsigned x = 0; x < other.size().x(); x++) {
@@ -51,14 +42,12 @@ void Image::insert_image_in_bounds(Image const& other, Util::Vector2u where)
     }
 }
 
-Image::Image(Util::Vector2u size)
-{
+Image::Image(Util::Vector2u size) {
     m_size = size;
     m_pixels = new Util::Color[pixel_count()];
 }
 
-Image::~Image()
-{
+Image::~Image() {
     delete[] m_pixels;
 }
 
