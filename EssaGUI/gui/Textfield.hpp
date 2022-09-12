660c03d6909efdc1cac99041d6673d65976846a2
Maciej <sppmacd@pm.me>
2022-09-12 13:48:49 +0200

    GUI: Load default Textfield font size from theme

--- EssaGUI/gui/Textfield.hpp
+++ EssaGUI/gui/Textfield.hpp
@@ -11,7 +11,7 @@ public:
     virtual void draw(GUI::Window& window) const override;
 
     CREATE_VALUE(Util::UString, content, "")
-    CREATE_VALUE(size_t, font_size, 20)
+    CREATE_VALUE(size_t, font_size, theme().label_font_size)
     CREATE_VALUE(Align, alignment, Align::CenterLeft)
     CREATE_VALUE(float, padding, 5)
 
