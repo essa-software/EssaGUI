c1e2f208840df1022d64f8d209d815fd405d8b29
Maciej <sppmacd@pm.me>
2022-09-12 13:44:08 +0200

    GUI: Make Align::Center the default alignment for TextButton

--- EssaGUI/gui/TextButton.hpp
+++ EssaGUI/gui/TextButton.hpp
@@ -13,7 +13,7 @@ public:
     CREATE_VALUE(Util::UString, active_content, "")
     CREATE_VALUE(llgl::opengl::Texture const*, image, nullptr)
     CREATE_VALUE(size_t, font_size, 20)
-    CREATE_VALUE(Align, alignment, Align::CenterLeft)
+    CREATE_VALUE(Align, alignment, Align::Center)
 
 private:
     virtual void draw(GUI::Window& window) const override;
