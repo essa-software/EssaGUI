54cb8d2b27782924f61be59e952b7acaf7614381
Maciej <sppmacd@pm.me>
2022-09-19 18:30:00 +0200

    GUi/Console: Draw only visible lines
    This makes a noticeable performance gain since text rendering is not
    well optimized in our framework.

--- EssaGUI/gui/Console.cpp
+++ EssaGUI/gui/Console.cpp
@@ -32,10 +32,16 @@ void Console::clear() {
 void Console::draw(GUI::Window& window) const {
     size_t s = 0;
     for (auto& line : m_lines) {
+        auto position = Util::Vector2f { 5, s * LINE_SPACING + 19 } + scroll_offset();
+        if (position.y() > raw_size().y() + LINE_SPACING || position.y() < 0) {
+            s++;
+            continue;
+        }
+
         TextDrawOptions options;
         options.fill_color = line.color;
         options.font_size = theme().label_font_size;
-        window.draw_text(line.text, Application::the().fixed_width_font(), Util::Vector2f { 5, s * LINE_SPACING + 19 } + scroll_offset(), options);
+        window.draw_text(line.text, Application::the().fixed_width_font(), position, options);
         s++;
     }
 
