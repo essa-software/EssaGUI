98ae7e6e6f61897065b29ce484553d3271a786f1
Maciej <sppmacd@pm.me>
2022-10-24 17:28:09 +0200

    LLGL: Add LineLoop primitive type

--- LLGL/OpenGL/PrimitiveType.hpp
+++ LLGL/OpenGL/PrimitiveType.hpp
@@ -9,6 +9,7 @@ enum class PrimitiveType {
     Points = GL_POINTS,
     Lines = GL_LINES,
     LineStrip = GL_LINE_STRIP,
+    LineLoop = GL_LINE_LOOP,
     Triangles = GL_TRIANGLES,
     TriangleStrip = GL_TRIANGLE_STRIP,
     TriangleFan = GL_TRIANGLE_FAN,
