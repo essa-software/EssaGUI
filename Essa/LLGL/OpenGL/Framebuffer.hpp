f45e6bef5c6fc8b048464ae478f0ee81295094c2
Maciej <sppmacd@pm.me>
2022-12-15 20:19:30 +0100

    LLGL: Add Framebuffer::fbo()

--- Essa/LLGL/OpenGL/Framebuffer.hpp
+++ Essa/LLGL/OpenGL/Framebuffer.hpp
@@ -16,26 +16,22 @@ public:
         : m_fbo(size)
         , m_renderer(m_fbo.id()) { }
 
-    void clear() {
-        m_renderer.clear();
-    }
+    void clear() { m_renderer.clear(); }
 
     template<class VertT, class DSS>
-    requires(std::is_same_v<typename DSS::Vertex, VertT>) void draw_vertices(VertexArray<VertT> const& vbo, DrawState<DSS> const& draw_state) {
+    requires(std::is_same_v<typename DSS::Vertex, VertT>) void draw_vertices(
+        VertexArray<VertT> const& vbo, DrawState<DSS> const& draw_state) {
         m_renderer.draw_vertices(vbo, draw_state);
     }
 
-    void resize(Util::Vector2u size) {
-        m_fbo.resize(size);
-    }
+    void resize(Util::Vector2u size) { m_fbo.resize(size); }
 
-    void set_label(std::string const& label) {
-        m_fbo.set_label(label);
-    }
+    void set_label(std::string const& label) { m_fbo.set_label(label); }
 
     auto const& color_texture() const { return m_fbo.color_texture(); }
 
     Renderer& renderer() { return m_renderer; }
+    auto const& fbo() const { return m_fbo; }
 
 private:
     llgl::opengl::FBO m_fbo;
