cd0e7338304fe7a617ab325caf35d98668b757ed
Maciej <sppmacd@pm.me>
2022-12-18 15:41:15 +0100

    GUI/Progressbar: Use ThemeRenderer

--- Essa/GUI/Widgets/Progressbar.cpp
+++ Essa/GUI/Widgets/Progressbar.cpp
@@ -21,14 +21,10 @@ Util::UString Progressbar::get_content_with_labelling() const {
     return m_content;
 }
 
-void Progressbar::draw(Gfx::Painter& window) const {
+void Progressbar::draw(Gfx::Painter& painter) const {
     auto theme_colors = theme().textbox.value(*this);
 
-    Gfx::RectangleDrawOptions background_rect;
-    background_rect.fill_color = theme_colors.background;
-    background_rect.outline_color = is_focused() ? theme().focus_frame : theme_colors.foreground;
-    background_rect.outline_thickness = -1;
-    window.draw_rectangle(local_rect(), background_rect);
+    theme().renderer().draw_text_editor_background(*this, painter);
 
     Util::Rectf completed_rect = local_rect();
     completed_rect.width = std::min((float)m_value / (float)m_max * raw_size().x(), raw_size().x() - 4);
@@ -37,13 +33,15 @@ void Progressbar::draw(Gfx::Painter& window) const {
     completed_rect.top = 2;
     Gfx::RectangleDrawOptions completed_opt;
     completed_opt.fill_color = m_progressbar_color;
-    window.draw_rectangle(completed_rect, completed_opt);
+    painter.draw_rectangle(completed_rect, completed_opt);
 
     Gfx::Text text_opt { get_content_with_labelling(), Application::the().font() };
     text_opt.set_font_size(raw_size().y() - 2);
     text_opt.set_fill_color(theme_colors.text);
     text_opt.align(Align::Center, local_rect());
-    text_opt.draw(window);
+    text_opt.draw(painter);
+
+    theme().renderer().draw_text_editor_border(*this, true, painter);
 }
 
 void Progressbar::step_by() {
