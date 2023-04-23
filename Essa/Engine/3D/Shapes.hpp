b2e49af32c0a06405d57f24c2af1f0528edd8d72
Maciej <sppmacd@pm.me>
2023-04-23 19:04:56 +0200

    Engine: Make add_wireframe_cube general
    For every Vertex that implements VertexMapping.

--- Essa/Engine/3D/Shapes.hpp
+++ Essa/Engine/3D/Shapes.hpp
@@ -4,6 +4,8 @@
 #include <EssaUtil/Color.hpp>
 #include <vector>
 
+#include "Essa/LLGL/OpenGL/MappedVertex.hpp"
+#include "Essa/LLGL/OpenGL/Vertex.hpp"
 #include "Model.hpp"
 
 namespace Essa::Shapes {
@@ -16,6 +18,43 @@ struct CubeProperties {
 };
 
 void add_cube(std::vector<Model::Vertex>& vertices, CubeProperties properties = {});
-void add_wireframe_cube(std::vector<Model::Vertex>& vertices);
+
+template<llgl::MappableVertex Vertex>
+void add_wireframe_cube(std::vector<Vertex>& vertices) {
+    auto add = [&](Util::Vector3f position) {
+        Vertex vertex;
+        llgl::MappedVertex<Vertex> mapping { vertex };
+        mapping.set_position(position);
+        mapping.set_color(Util::Colors::White);
+        vertices.push_back(vertex);
+    };
+
+    add({ -1, -1, -1 });
+    add({ 1, -1, -1 });
+    add({ -1, 1, -1 });
+    add({ 1, 1, -1 });
+    add({ -1, -1, 1 });
+    add({ 1, -1, 1 });
+    add({ -1, 1, 1 });
+    add({ 1, 1, 1 });
+
+    add({ -1, -1, -1 });
+    add({ -1, 1, -1 });
+    add({ 1, -1, -1 });
+    add({ 1, 1, -1 });
+    add({ -1, -1, 1 });
+    add({ -1, 1, 1 });
+    add({ 1, -1, 1 });
+    add({ 1, 1, 1 });
+
+    add({ -1, -1, -1 });
+    add({ -1, -1, 1 });
+    add({ 1, -1, -1 });
+    add({ 1, -1, 1 });
+    add({ -1, 1, -1 });
+    add({ -1, 1, 1 });
+    add({ 1, 1, -1 });
+    add({ 1, 1, 1 });
+}
 
 }
