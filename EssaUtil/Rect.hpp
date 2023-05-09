c909a2dd136142538579c1e81ca4a605b8339842
Maciej <sppmacd@pm.me>
2023-05-09 20:55:20 +0200

    Util: Make Rect::centered not a template
    Require users to cast coordinates before. This solves compile errors
    when U != T.

--- EssaUtil/Rect.hpp
+++ EssaUtil/Rect.hpp
@@ -42,9 +42,8 @@ public:
     template<class U>
     Rect<U> cast() const { return Rect<U>(static_cast<U>(left), static_cast<U>(top), static_cast<U>(width), static_cast<U>(height)); }
 
-    template<class U>
-    static Rect centered(Cs::Point2<U> center, Cs::Size2<U> size) {
-        return Rect { center - size.to_vector() / U { 2 }, size };
+    static Rect centered(Cs::Point2<T> center, Cs::Size2<T> size) {
+        return Rect { center - size.to_vector() / T { 2 }, size };
     }
 
     Cs::Point2<T> position() const { return { left, top }; }
