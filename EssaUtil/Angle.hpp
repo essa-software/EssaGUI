b725c00a7393b6468adace44211ebde3f7e9f38e
Maciej <sppmacd@pm.me>
2023-05-13 21:28:13 +0200

    Util: Add fmt formatter for Angle

--- EssaUtil/Angle.hpp
+++ EssaUtil/Angle.hpp
@@ -2,20 +2,16 @@
 
 #include <cmath>
 #include <compare>
+#include <fmt/core.h>
 #include <ostream>
 
 namespace Util {
 
-template<class T>
-constexpr T degrees_in_radian = 180 / M_PI;
+template<class T> constexpr T degrees_in_radian = 180 / M_PI;
 
-constexpr auto rad_to_deg(auto radians) {
-    return radians * degrees_in_radian<decltype(radians)>;
-}
+constexpr auto rad_to_deg(auto radians) { return radians * degrees_in_radian<decltype(radians)>; }
 
-constexpr auto deg_to_rad(auto degrees) {
-    return degrees / degrees_in_radian<decltype(degrees)>;
-}
+constexpr auto deg_to_rad(auto degrees) { return degrees / degrees_in_radian<decltype(degrees)>; }
 
 class Angle {
 public:
@@ -27,41 +23,24 @@ public:
     constexpr float deg() const { return rad_to_deg(m_value_in_radians); }
     constexpr float rad() const { return m_value_in_radians; }
 
-    constexpr Angle operator-() const {
-        return Angle(-m_value_in_radians);
-    }
+    constexpr Angle operator-() const { return Angle(-m_value_in_radians); }
 
-    constexpr Angle operator+(Angle const& other) const {
-        return Angle { m_value_in_radians + other.m_value_in_radians };
-    }
+    constexpr Angle operator+(Angle const& other) const { return Angle { m_value_in_radians + other.m_value_in_radians }; }
 
-    constexpr Angle operator-(Angle const& other) const {
-        return Angle { m_value_in_radians - other.m_value_in_radians };
-    }
+    constexpr Angle operator-(Angle const& other) const { return Angle { m_value_in_radians - other.m_value_in_radians }; }
 
-    constexpr Angle operator*(float n) const {
-        return Angle { m_value_in_radians * n };
-    }
+    constexpr Angle operator*(float n) const { return Angle { m_value_in_radians * n }; }
 
-    constexpr Angle& operator+=(Angle const& other) {
-        return *this = *this + other;
-    }
+    constexpr Angle& operator+=(Angle const& other) { return *this = *this + other; }
 
-    constexpr Angle& operator-=(Angle const& other) {
-        return *this = *this - other;
-    }
+    constexpr Angle& operator-=(Angle const& other) { return *this = *this - other; }
 
-    constexpr Angle& operator*=(float n) {
-        return *this = *this * n;
-    }
+    constexpr Angle& operator*=(float n) { return *this = *this * n; }
 
     constexpr std::partial_ordering operator<=>(Angle const& other) const { return m_value_in_radians <=> other.m_value_in_radians; }
     constexpr bool operator==(Angle const& other) const = default;
 
-    friend std::ostream&
-    operator<<(std::ostream& out, Angle alfa) {
-        return out << alfa.rad() << " [rad]";
-    }
+    friend std::ostream& operator<<(std::ostream& out, Angle alfa) { return out << alfa.rad() << " [rad]"; }
 
 private:
     constexpr Angle(float rads)
@@ -72,16 +51,15 @@ private:
 
 }
 
-constexpr Util::Angle operator""_deg(long double v) {
-    return Util::Angle::degrees(v);
-}
-constexpr Util::Angle operator""_deg(unsigned long long v) {
-    return Util::Angle::degrees(v);
-}
+constexpr Util::Angle operator""_deg(long double v) { return Util::Angle::degrees(v); }
+constexpr Util::Angle operator""_deg(unsigned long long v) { return Util::Angle::degrees(v); }
 
-constexpr Util::Angle operator""_rad(long double v) {
-    return Util::Angle::radians(v);
-}
-constexpr Util::Angle operator""_rad(unsigned long long v) {
-    return Util::Angle::radians(v);
-}
+constexpr Util::Angle operator""_rad(long double v) { return Util::Angle::radians(v); }
+constexpr Util::Angle operator""_rad(unsigned long long v) { return Util::Angle::radians(v); }
+
+template<> class fmt::formatter<Util::Angle> : public fmt::formatter<std::string_view> {
+public:
+    template<typename FormatContext> constexpr auto format(Util::Angle const& p, FormatContext& ctx) const {
+        return fmt::format_to(ctx.out(), "{}°", p.deg());
+    }
+};
