a6f46ed6e2df2a598341011cf09ff7d6beed0dc6
Maciej <sppmacd@pm.me>
2023-05-22 20:57:02 +0200

    Util: Support Point as coords for MultidimensionalArray

--- EssaUtil/MultidimensionalArray.hpp
+++ EssaUtil/MultidimensionalArray.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "CoordinateSystem.hpp"
 #include "NonCopyable.hpp"
 #include "Vector.hpp"
+
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
@@ -10,17 +12,14 @@
 
 namespace Util {
 
-template<class T, size_t... Dimensions>
-class MultidimensionalArray {
+template<class T, size_t... Dimensions> class MultidimensionalArray {
 public:
+    using Self = MultidimensionalArray<T, Dimensions...>;
     static constexpr size_t Rank = sizeof...(Dimensions);
     using Type = T;
     using Vector = Detail::DeprecatedVector<Rank, size_t>;
 
-    template<size_t Dim>
-    static size_t dimension() {
-        return std::get<Dim>(std::tuple { Dimensions... });
-    }
+    template<size_t Dim> static size_t dimension() { return std::get<Dim>(std::tuple { Dimensions... }); }
 
     MultidimensionalArray(Type const& v = {}) {
         m_storage = new Type[size()];
@@ -30,9 +29,7 @@ public:
     MultidimensionalArray(MultidimensionalArray const&) = delete;
     MultidimensionalArray& operator=(MultidimensionalArray const&) = delete;
 
-    MultidimensionalArray(MultidimensionalArray&& arr) {
-        *this = std::move(arr);
-    }
+    MultidimensionalArray(MultidimensionalArray&& arr) { *this = std::move(arr); }
     MultidimensionalArray& operator=(MultidimensionalArray&& arr) {
         if (this == &arr)
             return *this;
@@ -47,23 +44,26 @@ public:
     Vector dimensions() const { return { Dimensions... }; }
     size_t size() const { return (Dimensions * ...); }
 
-    void fill(Type const& fill) {
-        std::fill(m_storage, m_storage + size(), fill);
-    }
+    void fill(Type const& fill) { std::fill(m_storage, m_storage + size(), fill); }
 
-    template<std::convertible_to<size_t>... Dim>
-    Type& ref(Dim... index) {
-        assert(((index < Dimensions) && ...));
-        return *cell(index...);
-    }
-
-    template<std::convertible_to<size_t>... Dim>
+    template<class... Dim>
+        requires(std::is_unsigned_v<Dim> && ...)
     Type const& ref(Dim... index) const {
         assert(((index < Dimensions) && ...));
         return *cell(index...);
     }
 
-    template<std::convertible_to<size_t>... Dim>
+    template<class... Dim>
+        requires(std::is_unsigned_v<Dim> && ...)
+    Type& ref(Dim... index) {
+        return const_cast<Type&>(const_cast<Self const*>(this)->ref(index...));
+    }
+
+    Type const& ref(Detail::Point<Rank, size_t> const& coords) const { return ref(coords, std::make_index_sequence<Rank>()); }
+    Type& ref(Detail::Point<Rank, size_t> const& coords) { return const_cast<Type&>(const_cast<Self const*>(this)->ref(coords)); }
+
+    template<class... Dim>
+        requires(std::is_unsigned_v<Dim> && ...)
     Type get(Dim... index) const {
         if (!m_storage) {
             // Array doesn't take memory but appears default-initialized here.
@@ -73,46 +73,50 @@ public:
         return *cell(index...);
     }
 
-    template<std::convertible_to<size_t>... Dim>
-    void set(Dim... index, T value) {
+    Type get(Detail::Point<Rank, size_t> const& coords) const { return get(coords, std::make_index_sequence<Rank>()); }
+
+    template<std::convertible_to<size_t>... Dim> void set(Dim... index, T value) {
         assert(((index < Dimensions) && ...));
         *cell(index...) = std::move(value);
     }
 
     template<class Callback>
-    requires(std::is_invocable_v<Callback, Vector, T&>) void for_each_cell(Callback&& callback) {
+        requires(std::is_invocable_v<Callback, Vector, T&>)
+    void for_each_cell(Callback&& callback) {
         for (size_t s = 0; s < size(); s++) {
             callback(index_to_coords(s), m_storage[s]);
         }
     }
 
     template<class Callback>
-    requires(std::is_invocable_v<Callback, Vector, T const&>) void for_each_cell(Callback&& callback) const {
+        requires(std::is_invocable_v<Callback, Vector, T const&>)
+    void for_each_cell(Callback&& callback) const {
         for (size_t s = 0; s < size(); s++) {
             callback(index_to_coords(s), m_storage[s]);
         }
     }
 
 private:
-    template<std::convertible_to<size_t>... Dim>
-    Type* cell(Dim... index) { return &m_storage[coords_to_index(index...)]; }
+    template<size_t... Idx> Type const& ref(Detail::Point<Rank, size_t> const& coords, std::index_sequence<Idx...>) const {
+        return ref(coords.component(Idx)...);
+    }
+    template<size_t... Idx> Type get(Detail::Point<Rank, size_t> const& coords, std::index_sequence<Idx...>) const {
+        return get(coords.component(Idx)...);
+    }
 
-    template<std::convertible_to<size_t>... Dim>
-    Type const* cell(Dim... index) const { return &m_storage[coords_to_index(index...)]; }
+    template<std::convertible_to<size_t>... Dim> Type* cell(Dim... index) { return &m_storage[coords_to_index(index...)]; }
 
-    template<std::convertible_to<size_t>... Dim>
-    constexpr size_t coords_to_index(Dim... index) const {
+    template<std::convertible_to<size_t>... Dim> Type const* cell(Dim... index) const { return &m_storage[coords_to_index(index...)]; }
+
+    template<std::convertible_to<size_t>... Dim> constexpr size_t coords_to_index(Dim... index) const {
         size_t idx = 0;
         ((idx *= Dimensions, idx += index), ...);
         return idx;
     }
 
-    constexpr Vector index_to_coords(size_t index) const {
-        return index_to_coords_impl(index, std::make_index_sequence<Rank>());
-    }
+    constexpr Vector index_to_coords(size_t index) const { return index_to_coords_impl(index, std::make_index_sequence<Rank>()); }
 
-    template<size_t... Seq>
-    constexpr Vector index_to_coords_impl(size_t index, std::index_sequence<Seq...>) const {
+    template<size_t... Seq> constexpr Vector index_to_coords_impl(size_t index, std::index_sequence<Seq...>) const {
         Vector out;
         ((out.components[Seq] = index % Dimensions, index /= Dimensions), ...);
         return out;
