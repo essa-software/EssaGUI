ace5344afbd5cda40cd36543bc4c71f4b9fe1c81
Maciej <sppmacd@pm.me>
2023-07-05 17:58:46 +0200

    Util/DisplayError: Output to stderr

--- EssaUtil/DisplayError.cpp
+++ EssaUtil/DisplayError.cpp
@@ -6,7 +6,7 @@
 namespace Util {
 
 void display_error(ReadableStream& input, DisplayedError error) {
-    fmt::print("\e[1;31mError:\e[m {}\n", error.message.encode());
+    fmt::print(stderr, "\e[1;31mError:\e[m {}\n", error.message.encode());
 
     TextReader reader { input };
     size_t last_index = 0;
@@ -18,7 +18,7 @@ void display_error(ReadableStream& input, DisplayedError error) {
 
         if (last_index <= error.start_offset && last_index + line.size() >= static_cast<size_t>(error.start_offset)) {
             auto line_number = reader.is_eof() ? reader.location().line : reader.location().line - 1;
-            fmt::print("{:4} | ", line_number + 1);
+            fmt::print(stderr, "{:4} | ", line_number + 1);
 
             size_t range_size = [&]() -> size_t {
                 if (error.end_offset < error.start_offset) {
@@ -32,18 +32,18 @@ void display_error(ReadableStream& input, DisplayedError error) {
 
             for (ssize_t s = 0; static_cast<size_t>(s) < line.size(); s++) {
                 if (s + last_index >= error.start_offset && s + last_index < error.start_offset + range_size)
-                    fmt::print("\e[31m{}\e[m", Util::UString { line.at(s) }.encode());
+                    fmt::print(stderr, "\e[31m{}\e[m", Util::UString { line.at(s) }.encode());
                 else
-                    fmt::print("{}", Util::UString { line.at(s) }.encode());
+                    fmt::print(stderr, "{}", Util::UString { line.at(s) }.encode());
             }
-            fmt::print("\n");
-            fmt::print("       ");
+            fmt::print(stderr, "\n");
+            fmt::print(stderr, "       ");
             for (size_t s = 0; s < error.start_offset - last_index; s++)
-                fmt::print(" ");
+                fmt::print(stderr, " ");
             for (size_t s = 0; s < range_size; s++) {
-                fmt::print("^");
+                fmt::print(stderr, "^");
             }
-            fmt::print(" \e[31m{}\e[m\n", error.message.encode());
+            fmt::print(stderr, " \e[31m{}\e[m\n", error.message.encode());
         }
 
         last_index = reader.location().offset;
