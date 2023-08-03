67cb639e3c0a030f3c2f524ea622ea6129c37229
Maciej <sppmacd@pm.me>
2023-08-03 13:49:52 +0200

    Tools: Don't use legacy MDI in EMLPlayground

--- EssaTools/EMLPlayground/main.cpp
+++ EssaTools/EMLPlayground/main.cpp
@@ -1,9 +1,12 @@
 #include <Essa/GUI/Application.hpp>
 #include <Essa/GUI/EML/SyntaxHighlighter.hpp>
 #include <Essa/GUI/Overlays/MessageBox.hpp>
+#include <Essa/GUI/Overlays/ToolWindow.hpp>
 #include <Essa/GUI/Widgets/Container.hpp>
+#include <Essa/GUI/Widgets/MDI/Host.hpp>
 #include <Essa/GUI/Widgets/TextButton.hpp>
 #include <Essa/GUI/Widgets/TextEditor.hpp>
+#include <Essa/GUI/WindowRoot.hpp>
 #include <EssaUtil/Error.hpp>
 #include <EssaUtil/GenericParser.hpp>
 
@@ -11,10 +14,10 @@ int main() {
     GUI::Application app;
     auto& host_window = app.create_host_window({ 500, 500 }, "EML Playground");
 
-    auto& container = host_window.set_main_widget<GUI::Container>();
+    auto& container = host_window.set_root_widget<GUI::Container>();
     container.set_layout<GUI::VerticalBoxLayout>();
 
-    auto toolbar = container.add_widget<GUI::Container>();
+    auto* toolbar = container.add_widget<GUI::Container>();
     toolbar->set_layout<GUI::HorizontalBoxLayout>();
     toolbar->set_size({ Util::Length::Auto, 32.0_px });
 
@@ -25,27 +28,31 @@ int main() {
     auto* preview = container.add_widget<GUI::Container>();
     preview->set_layout<GUI::HorizontalBoxLayout>();
 
-    auto eml_editor = preview->add_widget<GUI::TextEditor>();
-    auto eml_container = preview->add_widget<GUI::Container>();
+    auto* eml_editor = preview->add_widget<GUI::TextEditor>();
+    auto* eml_container = preview->add_widget<GUI::MDI::Host>();
 
-    GUI::ToolWindow* tool_window = nullptr;
+    GUI::WindowRoot* tool_window = nullptr;
     eml_editor->on_change = [&](Util::UString const& text) {
-        eml_container->shrink(0);
-        (void)eml_container->load_from_eml_string(text.encode());
+        auto& container = eml_container->set_background_widget<GUI::Container>();
+        (void)container.load_from_eml_string(text.encode());
         if (tool_window) {
             (void)tool_window->load_from_eml_string(text.encode());
-            tool_window->set_position({ host_window.size().x() * 3 / 4 - tool_window->size().x() / 2,
-                                        host_window.size().y() / 2 - tool_window->size().y() / 2 });
         }
     };
     eml_editor->set_syntax_highlighter(std::make_unique<EML::SyntaxHighlighter>());
 
     open_window->on_click = [&]() {
-        tool_window = &host_window.open_overlay<GUI::ToolWindow>();
-        (void)tool_window->load_from_eml_string(eml_editor->content().encode());
-        tool_window->set_position({ host_window.size().x() * 3 / 4 - tool_window->size().x() / 2,
-                                    host_window.size().y() / 2 - tool_window->size().y() / 2 });
-        tool_window->on_close = [&tool_window]() { tool_window = nullptr; };
+        if (tool_window) {
+            tool_window->window().close();
+        }
+        auto opened_overlay = eml_container->open_overlay();
+        (void)opened_overlay.window_root.load_from_eml_string(eml_editor->content().encode());
+        tool_window = &opened_overlay.window_root;
+        opened_overlay.overlay.on_close = [&tool_window, new_window_root = &opened_overlay.window_root]() {
+            if (tool_window == new_window_root) {
+                tool_window = nullptr;
+            }
+        };
     };
 
     app.run();
