698dfe79493fcf5557783783d91cf8152e8dab91
Maciej <sppmacd@pm.me>
2023-08-03 15:20:27 +0200

    GUI: Don't use legacy MDI in scrollable-widget
    Don't use MDI at all since it is not needed for this test

--- examples/essa-gui/scrollable-widget.cpp
+++ examples/essa-gui/scrollable-widget.cpp
@@ -1,26 +1,21 @@
 #include <Essa/GUI/Application.hpp>
+#include <Essa/GUI/Overlays/ToolWindow.hpp>
 #include <Essa/GUI/Widgets/Console.hpp>
 #include <Essa/GUI/Widgets/Container.hpp>
-#include <Essa/GUI/Overlays/ToolWindow.hpp>
 
 int main() {
     GUI::Application app;
-    auto& host_window = app.create_host_window({ 1000, 1000 }, "Text Editor");
+    auto& host_window = app.create_host_window({ 1000, 1000 }, "Scrollable Widget");
 
-    auto& wnd1 = host_window.open_overlay<GUI::ToolWindow>();
-    wnd1.set_size({ 500, 100 });
-    wnd1.center_on_screen();
-    auto& container1 = wnd1.set_main_widget<GUI::Container>();
-    container1.set_layout<GUI::HorizontalBoxLayout>();
-    auto console = container1.add_widget<GUI::Console>();
-    console->append_line({ .color = Util::Colors::Red, .text = "test1" });
-    console->append_line({ .color = Util::Colors::Yellow, .text = "test2" });
-    console->append_line({ .color = Util::Colors::Green, .text = "test3" });
-    console->append_line({ .color = Util::Colors::Blue, .text = "test4" });
-    console->append_line({ .color = Util::Colors::Red, .text = "test5" });
-    console->append_line({ .color = Util::Colors::Yellow, .text = "test6" });
-    console->append_line({ .color = Util::Colors::Green, .text = "test7" });
-    console->append_line({ .color = Util::Colors::Blue, .text = "test8" });
+    auto& console = host_window.set_root_widget<GUI::Console>();
+    console.append_line({ .color = Util::Colors::Red, .text = "test1" });
+    console.append_line({ .color = Util::Colors::Yellow, .text = "test2" });
+    console.append_line({ .color = Util::Colors::Green, .text = "test3" });
+    console.append_line({ .color = Util::Colors::Blue, .text = "test4" });
+    console.append_line({ .color = Util::Colors::Red, .text = "test5" });
+    console.append_line({ .color = Util::Colors::Yellow, .text = "test6" });
+    console.append_line({ .color = Util::Colors::Green, .text = "test7" });
+    console.append_line({ .color = Util::Colors::Blue, .text = "test8" });
 
     app.run();
     return 0;
