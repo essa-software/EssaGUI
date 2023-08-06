9be31a7c16c77ec52a4561726dc5c65de51b7e17
Maciej <sppmacd@pm.me>
2023-08-06 20:10:29 +0200

    LLGL: Remove "command buffer" from AbstractOpenGLHelper
    - This was dead code
    - This used manual memory management

--- Essa/AbstractOpenGLHelper.cpp
+++
@@ -1,4 +0,0 @@
-#include "AbstractOpenGLHelper.hpp"
-
-std::list<OpenGLCommand> OpenGL::command_buffer;
-bool OpenGL::write_commands_to_buffer = false;
--- Essa/AbstractOpenGLHelper.hpp
+++ Essa/AbstractOpenGLHelper.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <GL/glew.h>
-#include <GL/gl.h>
 
+#include <GL/gl.h>
 #include <array>
 #include <functional>
 #include <list>
@@ -10,96 +10,71 @@
 #include <tuple>
 #include <vector>
 
-struct OpenGLCommand {
-    std::string name;
-    void* params = nullptr;
-    std::function<void(void*)> destroy;
-};
-
-#define ADD_OPENGL_FUNCTION(FuncName, ShouldWrite)                                                             \
-    template<typename... Args> static auto FuncName(Args... args) {                                            \
-        if (ShouldWrite && write_commands_to_buffer) {                                                         \
-            OpenGLCommand cmd { .name = "gl" + std::string(#FuncName),                                         \
-                                .params = new std::tuple<Args...>(args...),                                    \
-                                .destroy = [](void* ptr) { delete static_cast<std::tuple<Args...>*>(ptr); } }; \
-            command_buffer.push_back(cmd);                                                                     \
-        }                                                                                                      \
-        return gl##FuncName(args...);                                                                          \
-    }
+#define ADD_OPENGL_FUNCTION(FuncName) \
+    template<typename... Args> static auto FuncName(Args... args) { return gl##FuncName(args...); }
 
 class OpenGL {
 public:
-    static std::list<OpenGLCommand> command_buffer;
-    static bool write_commands_to_buffer;
+    ADD_OPENGL_FUNCTION(ActiveTexture)
+    ADD_OPENGL_FUNCTION(BindTexture)
+    ADD_OPENGL_FUNCTION(DeleteTextures)
+    ADD_OPENGL_FUNCTION(GenTextures)
+    ADD_OPENGL_FUNCTION(TexImage2D)
+    ADD_OPENGL_FUNCTION(FramebufferTexture)
+    ADD_OPENGL_FUNCTION(GetTexImage)
 
-    ADD_OPENGL_FUNCTION(ActiveTexture, true)
-    ADD_OPENGL_FUNCTION(BindTexture, true)
-    ADD_OPENGL_FUNCTION(DeleteTextures, true)
-    ADD_OPENGL_FUNCTION(GenTextures, true)
-    ADD_OPENGL_FUNCTION(TexImage2D, false)
-    ADD_OPENGL_FUNCTION(FramebufferTexture, false)
-    ADD_OPENGL_FUNCTION(GetTexImage, true)
+    ADD_OPENGL_FUNCTION(BufferData)
+    ADD_OPENGL_FUNCTION(BindBuffer)
+    ADD_OPENGL_FUNCTION(BindFramebuffer)
+    ADD_OPENGL_FUNCTION(BindRenderbuffer)
+    ADD_OPENGL_FUNCTION(DrawBuffers)
+    ADD_OPENGL_FUNCTION(DeleteBuffers)
+    ADD_OPENGL_FUNCTION(DeleteFramebuffers)
+    ADD_OPENGL_FUNCTION(DeleteRenderbuffers)
+    ADD_OPENGL_FUNCTION(FramebufferRenderbuffer)
+    ADD_OPENGL_FUNCTION(GenFramebuffers)
+    ADD_OPENGL_FUNCTION(GenRenderbuffers)
+    ADD_OPENGL_FUNCTION(GenBuffers)
+    ADD_OPENGL_FUNCTION(CheckFramebufferStatus)
+    ADD_OPENGL_FUNCTION(RenderbufferStorage)
 
-    ADD_OPENGL_FUNCTION(BufferData, true)
-    ADD_OPENGL_FUNCTION(BindBuffer, true)
-    ADD_OPENGL_FUNCTION(BindFramebuffer, true)
-    ADD_OPENGL_FUNCTION(BindRenderbuffer, true)
-    ADD_OPENGL_FUNCTION(DrawBuffers, true)
-    ADD_OPENGL_FUNCTION(DeleteBuffers, true)
-    ADD_OPENGL_FUNCTION(DeleteFramebuffers, true)
-    ADD_OPENGL_FUNCTION(DeleteRenderbuffers, true)
-    ADD_OPENGL_FUNCTION(FramebufferRenderbuffer, true)
-    ADD_OPENGL_FUNCTION(GenFramebuffers, true)
-    ADD_OPENGL_FUNCTION(GenRenderbuffers, true)
-    ADD_OPENGL_FUNCTION(GenBuffers, true)
-    ADD_OPENGL_FUNCTION(CheckFramebufferStatus, false)
-    ADD_OPENGL_FUNCTION(RenderbufferStorage, false)
+    ADD_OPENGL_FUNCTION(BindVertexArray)
+    ADD_OPENGL_FUNCTION(DeleteVertexArrays)
+    ADD_OPENGL_FUNCTION(DrawElements)
+    ADD_OPENGL_FUNCTION(DrawArrays)
+    ADD_OPENGL_FUNCTION(GenVertexArrays)
+    ADD_OPENGL_FUNCTION(VertexAttribPointer)
+    ADD_OPENGL_FUNCTION(EnableVertexAttribArray)
 
-    ADD_OPENGL_FUNCTION(BindVertexArray, true)
-    ADD_OPENGL_FUNCTION(DeleteVertexArrays, true)
-    ADD_OPENGL_FUNCTION(DrawElements, true)
-    ADD_OPENGL_FUNCTION(DrawArrays, true)
-    ADD_OPENGL_FUNCTION(GenVertexArrays, true)
-    ADD_OPENGL_FUNCTION(VertexAttribPointer, true)
-    ADD_OPENGL_FUNCTION(EnableVertexAttribArray, true)
+    ADD_OPENGL_FUNCTION(Clear)
+    ADD_OPENGL_FUNCTION(ClearColor)
+    ADD_OPENGL_FUNCTION(Viewport)
 
-    ADD_OPENGL_FUNCTION(Clear, true)
-    ADD_OPENGL_FUNCTION(ClearColor, true)
-    ADD_OPENGL_FUNCTION(Viewport, true)
+    ADD_OPENGL_FUNCTION(Enable)
+    ADD_OPENGL_FUNCTION(Disable)
+    ADD_OPENGL_FUNCTION(BlendFuncSeparate)
+    ADD_OPENGL_FUNCTION(GetError)
 
-    ADD_OPENGL_FUNCTION(Enable, true)
-    ADD_OPENGL_FUNCTION(Disable, true)
-    ADD_OPENGL_FUNCTION(BlendFuncSeparate, false)
-    ADD_OPENGL_FUNCTION(GetError, false)
+    ADD_OPENGL_FUNCTION(CreateShader)
+    ADD_OPENGL_FUNCTION(CompileShader)
+    ADD_OPENGL_FUNCTION(DeleteShader)
+    ADD_OPENGL_FUNCTION(GetShaderiv)
+    ADD_OPENGL_FUNCTION(GetShaderInfoLog)
+    ADD_OPENGL_FUNCTION(ShaderSource)
 
-    ADD_OPENGL_FUNCTION(CreateShader, false)
-    ADD_OPENGL_FUNCTION(CompileShader, false)
-    ADD_OPENGL_FUNCTION(DeleteShader, false)
-    ADD_OPENGL_FUNCTION(GetShaderiv, false)
-    ADD_OPENGL_FUNCTION(GetShaderInfoLog, false)
-    ADD_OPENGL_FUNCTION(ShaderSource, false)
+    ADD_OPENGL_FUNCTION(CreateProgram)
+    ADD_OPENGL_FUNCTION(DeleteProgram)
+    ADD_OPENGL_FUNCTION(GetProgramiv)
+    ADD_OPENGL_FUNCTION(GetProgramInfoLog)
+    ADD_OPENGL_FUNCTION(LinkProgram)
+    ADD_OPENGL_FUNCTION(ObjectLabel)
+    ADD_OPENGL_FUNCTION(UseProgram)
 
-    ADD_OPENGL_FUNCTION(CreateProgram, false)
-    ADD_OPENGL_FUNCTION(DeleteProgram, false)
-    ADD_OPENGL_FUNCTION(GetProgramiv, false)
-    ADD_OPENGL_FUNCTION(GetProgramInfoLog, false)
-    ADD_OPENGL_FUNCTION(LinkProgram, false)
-    ADD_OPENGL_FUNCTION(ObjectLabel, false)
-    ADD_OPENGL_FUNCTION(UseProgram, false)
-
-    ADD_OPENGL_FUNCTION(GetUniformLocation, false)
-    ADD_OPENGL_FUNCTION(Uniform1i, true)
-    ADD_OPENGL_FUNCTION(Uniform4f, true)
-    ADD_OPENGL_FUNCTION(UniformMatrix4fv, true)
-    ADD_OPENGL_FUNCTION(Uniform2f, true)
-    ADD_OPENGL_FUNCTION(Uniform3f, true)
-    ADD_OPENGL_FUNCTION(Uniform1f, true)
-
-    static auto clear() {
-        for (auto& cmd : command_buffer) {
-            cmd.destroy(cmd.params);
-        }
-
-        command_buffer.clear();
-    }
+    ADD_OPENGL_FUNCTION(GetUniformLocation)
+    ADD_OPENGL_FUNCTION(Uniform1i)
+    ADD_OPENGL_FUNCTION(Uniform4f)
+    ADD_OPENGL_FUNCTION(UniformMatrix4fv)
+    ADD_OPENGL_FUNCTION(Uniform2f)
+    ADD_OPENGL_FUNCTION(Uniform3f)
+    ADD_OPENGL_FUNCTION(Uniform1f)
 };
