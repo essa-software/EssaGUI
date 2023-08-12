3b6d6436207ce4c1e0aa793adcb1c62be2a3f4ec
Maciej <sppmacd@pm.me>
2023-08-12 20:04:42 +0200

    Examples/gallery: Display message box with selected file

--- examples/essa-gui/gallery.cpp
+++ examples/essa-gui/gallery.cpp
@@ -1,5 +1,6 @@
 #include <Essa/GUI/Application.hpp>
 #include <Essa/GUI/Overlays/FileExplorer.hpp>
+#include <Essa/GUI/Overlays/MessageBox.hpp>
 #include <Essa/GUI/Overlays/Prompt.hpp>
 #include <Essa/GUI/Widgets/Container.hpp>
 #include <Essa/GUI/Widgets/TextButton.hpp>
@@ -9,7 +10,18 @@ int main() {
     (void)app.main_widget().load_from_eml_resource(app.resource_manager().require<EML::EMLResource>("Gallery.eml"));
 
     auto& file_explorer = app.main_widget().find<GUI::TextButton>("file_explorer");
-    file_explorer.on_click = []() { GUI::FileExplorer::get_path_to_open(); };
+    file_explorer.on_click = []() {
+        auto path = GUI::FileExplorer::get_path_to_open();
+        if (path) {
+            GUI::message_box(
+                Util::UString::format("Selected path: {}", path->string()), "Info",
+                {
+                    .buttons = GUI::MessageBox::Buttons::Ok,
+                    .icon = GUI::MessageBox::Icon::Info,
+                }
+            );
+        }
+    };
     auto& prompt = app.main_widget().find<GUI::TextButton>("prompt");
     prompt.on_click = [&]() { GUI::prompt(app.window(), "Test prompt"); };
 
