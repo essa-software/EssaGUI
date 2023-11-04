f4e47fc671d2a6382408a56f66811dd01ff8e30e
Maciej <sppmacd@pm.me>
2023-11-04 21:30:41 +0100

    Examples: Use simple-app as a simple EML testbed

--- examples/essa-gui/simple-app.cpp
+++ examples/essa-gui/simple-app.cpp
@@ -1,11 +1,16 @@
 #include <Essa/GUI/Application.hpp>
+#include <Essa/GUI/Widgets/Container.hpp>
 #include <Essa/GUI/Widgets/Textfield.hpp>
 
+class MainWidget : public GUI::Container {
+public:
+    virtual void on_init() {
+        (void)load_from_eml_resource(resource_manager().require<EML::EMLResource>("MainWidget.eml")); //
+    }
+};
+
 int main() {
-    GUI::SimpleApplication<GUI::Textfield> app { "Simple app", { 500, 500 } };
-    auto& label = app.main_widget();
-    label.set_content("test");
-    label.set_alignment(GUI::Align::Center);
+    GUI::SimpleApplication<MainWidget> app { "Simple app", { 500, 500 } };
     app.run();
     return 0;
 }
