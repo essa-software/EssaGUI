1add5c221d0bd95f59839ffaa9fab2007ce0c924
Maciej <sppmacd@pm.me>
2023-11-05 20:59:34 +0100

    Examples: Tweak heighway benchmark
    - Move size to separate variablei
    - Use 750px everywhere

--- examples/essa-gui/heighway.cpp
+++ examples/essa-gui/heighway.cpp
@@ -9,6 +9,8 @@
 
 using namespace std::chrono_literals;
 
+constexpr float Size = 750;
+
 class MainWidget : public GUI::Widget {
 private:
     virtual void update() override {
@@ -24,7 +26,7 @@ private:
     virtual void draw(Gfx::Painter& painter) const override {
         Gfx::RectangleDrawOptions rect;
         rect.texture = &m_framebuffer.color_texture();
-        rect.texture_rect = { 0, 0, 500, 500 };
+        rect.texture_rect = { 0, 0, static_cast<unsigned int>(Size), static_cast<unsigned int>(Size) };
         painter.deprecated_draw_rectangle(local_rect().cast<float>(), rect);
 
         Gfx::Text text {
@@ -41,7 +43,7 @@ private:
     }
 
     void append_point() {
-        bool equation = Util::Random::default_engine().next_bool(2);
+        bool equation = Util::Random::default_engine().next_bool(5);
         auto old_position = m_current_position;
         if (equation) {
             m_current_position.set_x(-0.4 * old_position.x() - 1);
@@ -52,23 +54,23 @@ private:
             m_current_position.set_y(0.4 * old_position.x() + 0.76 * old_position.y());
         }
 
-        m_painter.set_projection(llgl::Projection { {}, { 0, 0, 500, 500 } });
+        m_painter.set_projection(llgl::Projection { {}, { 0, 0, static_cast<int>(Size), static_cast<int>(Size) } });
 
         m_rectangle.set_transform(llgl::Transform());
         m_painter.draw(m_rectangle.move(m_current_position.to_vector()));
     }
 
-    Gfx::Drawing::Rectangle m_rectangle { { 1 / 500.f, 1 / 500.f }, Gfx::Drawing::Fill::solid(Util::Colors::White) };
-    llgl::Framebuffer m_framebuffer { { 500, 500 } };
+    Gfx::Drawing::Rectangle m_rectangle { { 1 / Size, 1 / Size }, Gfx::Drawing::Fill::solid(Util::Colors::White) };
+    llgl::Framebuffer m_framebuffer { { Size, Size } };
     Gfx::Painter m_painter { m_framebuffer.renderer() };
-    Util::Point2f m_current_position;
+    Util::Point2f m_current_position {};
     uint64_t m_point_count = 0;
     Util::Clock m_clock;
 };
 
 int main() {
     GUI::Application app;
-    auto& wnd = app.create_host_window({ 750, 750 }, "Heighway's Dragon (Gfx::Painter benchmark)");
+    auto& wnd = app.create_host_window({ Size, Size }, "Heighway's Dragon (Gfx::Painter benchmark)");
     wnd.set_root_widget<MainWidget>();
 
     app.run();
