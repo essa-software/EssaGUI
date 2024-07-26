9c90b0ca713574d280a700cb180cfccf2b28ad68
Maciej <sppmacd@pm.me>
2024-07-26 15:01:16 +0200

    Build: Update tinyvg

--- cmake/tinyvg.cmake
+++ cmake/tinyvg.cmake
@@ -1,6 +1,5 @@
 option(ESSA_ZIG_PATH "Path to the zig executable" "")
 
-# check if zig version is exactly 0.11.0
 execute_process(COMMAND "${ESSA_ZIG_PATH}" version
     OUTPUT_VARIABLE ZIG_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -10,8 +9,8 @@ if(NOT ZIG_VERSION)
     message(FATAL_ERROR "Zig not found, please set ESSA_ZIG_PATH CMake variable to the zig executable")
 endif()
 
-if(NOT ZIG_VERSION STREQUAL "0.11.0")
-    message(FATAL_ERROR "Zig version =0.11.0 is required, found ${ZIG_VERSION}")
+if(NOT ZIG_VERSION STREQUAL "0.13.0")
+    message(FATAL_ERROR "Zig version =0.13.0 is required, found ${ZIG_VERSION}")
 endif()
 
 include(ExternalProject)
@@ -21,9 +20,9 @@ if(NOT UNIX)
     message(FATAL_ERROR "TinyVG is only supported on Unix-like systems")
 endif()
 
-ExternalProject_Add(tinyvg_build
+ExternalProject_Add(tinyvg_ext
     GIT_REPOSITORY    git@github.com:TinyVG/sdk.git
-    GIT_TAG           24abddf4628b65f2d9d72522562659fe6639d609
+    GIT_TAG           d103ec3ec461a2f96b8a7e7c1dda8ff54d4c8825
     GIT_SHALLOW       TRUE
     GIT_PROGRESS      TRUE
     CONFIGURE_COMMAND ""
@@ -33,17 +32,16 @@ ExternalProject_Add(tinyvg_build
     BUILD_BYPRODUCTS  zig-out/lib/libtinyvg.so
 )
 
-ExternalProject_Get_Property(tinyvg_build SOURCE_DIR)
+# get source dir
+ExternalProject_Get_Property(tinyvg_ext SOURCE_DIR)
 
-set(INCLUDE_DIR ${SOURCE_DIR}/zig-out/include)
-# Hack to make it work, otherwise CMake will complain about missing
-# include directory.
-# (See https://stackoverflow.com/a/47358004)
-file(MAKE_DIRECTORY ${INCLUDE_DIR})
+set(tinyvg_path ${SOURCE_DIR}/zig-out/lib/libtinyvg.so)
 
-add_library(tinyvg INTERFACE)
-target_include_directories(tinyvg INTERFACE ${INCLUDE_DIR})
-target_link_libraries(tinyvg INTERFACE ${SOURCE_DIR}/zig-out/lib/libtinyvg.so)
-add_dependencies(tinyvg tinyvg_build)
+add_library(tinyvg SHARED IMPORTED GLOBAL)
+add_dependencies(tinyvg tinyvg_ext)
+set_target_properties(tinyvg
+    PROPERTIES IMPORTED_LOCATION ${tinyvg_path}
+)
+target_include_directories(tinyvg INTERFACE ${SOURCE_DIR}/src/binding/include)
 
-install(FILES ${SOURCE_DIR}/zig-out/lib/libtinyvg.so DESTINATION lib)
+install(FILES ${tinyvg_path} DESTINATION lib)
